da4398359541807bed554a0e592f1861ade6b916
Michal Maršálek <MichalMarsalek@users.noreply.github.com>
2025-04-28 20:04:44 +0200

    add vscode highlighting

--- vscode/side-by-side/src/extension.ts
+++ vscode/side-by-side/src/extension.ts
@@ -20,22 +20,86 @@ export async function activate(context: vscode.ExtensionContext) {
 
   const selector = { language: "side-by-side", scheme: "file" };
 
-  const provider: vscode.DocumentFormattingEditProvider = {
+  const formattingProvider: vscode.DocumentFormattingEditProvider = {
     provideDocumentFormattingEdits(
       document: vscode.TextDocument
     ): vscode.TextEdit[] {
+      const documentText = document.getText();
       const fullRange = new vscode.Range(
         document.positionAt(0),
-        document.positionAt(document.getText().length)
+        document.positionAt(documentText.length)
       );
 
-      const formatted = formatSbs(document.getText());
+      const formatted = formatSbs(documentText);
 
       return [vscode.TextEdit.replace(fullRange, formatted)];
     },
   };
 
   context.subscriptions.push(
-    vscode.languages.registerDocumentFormattingEditProvider(selector, provider)
+    vscode.languages.registerDocumentFormattingEditProvider(
+      selector,
+      formattingProvider
+    )
+  );
+
+  const tokensProvider: vscode.DocumentSemanticTokensProvider = {
+    provideDocumentSemanticTokens(
+      document: vscode.TextDocument
+    ): vscode.SemanticTokens {
+      const text = document.getText();
+      const lines = text.split(/\r?\n/);
+      const cst = sbs.deserialize(text);
+
+      const tokensBuilder = new vscode.SemanticTokensBuilder(sbsLegend);
+      let lineIndex = 0;
+      if (cst.isExplicitSeparator) {
+        tokensBuilder.push(lineIndex++, 0, cst.separator.trim().length, 1);
+      }
+      for (let pairIndex = 0; pairIndex <= cst.pairs.length; pairIndex++) {
+        const comment = cst.comments[pairIndex];
+        if (comment) {
+          const commentLines = comment.split(/\r?\n/);
+          for (const commentLine of commentLines) {
+            tokensBuilder.push(lineIndex++, 0, commentLine.length, 2);
+          }
+        }
+        const separatorLocations = cst.separatorLocations[pairIndex];
+        if (separatorLocations) {
+          for (const separatorLocation of separatorLocations) {
+            tokensBuilder.push(lineIndex, 0, separatorLocation, 0);
+            tokensBuilder.push(
+              lineIndex,
+              separatorLocation,
+              cst.separator.length,
+              1
+            );
+            tokensBuilder.push(
+              lineIndex,
+              separatorLocation + cst.separator.length,
+              lines[lineIndex].length -
+                separatorLocation +
+                cst.separator.length,
+              0
+            );
+            lineIndex++;
+          }
+        }
+      }
+
+      return tokensBuilder.build();
+    },
+  };
+
+  vscode.languages.registerDocumentSemanticTokensProvider(
+    selector,
+    tokensProvider,
+    sbsLegend
   );
 }
+
+const sbsLegend = new vscode.SemanticTokensLegend([
+  "string",
+  "operator",
+  "comment",
+]);
